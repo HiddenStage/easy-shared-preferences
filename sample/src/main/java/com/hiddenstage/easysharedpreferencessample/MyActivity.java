3d41b706ec7cf64ad4f5c62cbeca4862a4478141
Aaron <aschuenemann@gmail.com>
2014-07-09 18:09:20 -0500

    remove menu

--- sample/src/main/java/com/hiddenstage/easysharedpreferencessample/MyActivity.java
+++ sample/src/main/java/com/hiddenstage/easysharedpreferencessample/MyActivity.java
@@ -84,24 +84,4 @@ public class MyActivity extends Activity {
         floatValue.setText(Float.toString(float_variable));
         longValue.setText(Long.toString(long_variable));
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
